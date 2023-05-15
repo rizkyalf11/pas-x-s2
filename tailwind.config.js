ec61b2902393186f140ca99d4baefcd6f8432639
ehancup <mondokskuy19@gmail.com>
2023-05-15 11:13:53 +0700

    versi html biasa (VHB)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        pigma : '#7F56DA',
+        navigator : '#02D05B'
+      },
+      fontFamily:{
+        "quicksand": ["Quicksand" , "sans-serif"],
+        "poppins" :["Poppins" , "sans-serif"],
+      },
+      aspectRatio:{
+        'rect' : '5  / 1.05',
+        'ress' : '5 / 1.5'
+      },
+      screens:{
+        'hp' : '540px',
+        'hpk' : '478px',
+        'hpsk' : '376px'
+      }
+    },
   },
   plugins: [],
 }
